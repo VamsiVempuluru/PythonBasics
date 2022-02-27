0469442936906bca02ebce5dad2226ee82fa0bff
VamsiVempuluru <vamsigoud1999@gmail.com>
2022-02-27 12:34:06 +0530

    practice taking input from user, string formatting

---
+++ input_example.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gpa = float(input("Enter your gpa: "))
+
+print(f"your name is {name}")
+print(f"your age is {age}")
+print(f"your gpa is {gpa}")
+
+
+print("your name is " + name + " your age is " + str(age) + " your gpa is " + str(gpa))
+
+print("your name is",name,"your age is",age,"your gpa is",gpa)
+
+print(f"your name is {name} your age is {age} your gpa is {gpa}")
+
